a8b564978cddbcbcf1812bad412fdb9fac2cefe9
flakessp <flakessp@gmail.com>
2019-02-08 13:48:35 +0300

    still not right

--- bloter/app.js
+++ bloter/app.js
@@ -1,7 +1,7 @@
 // BLOTTER - Example 2
-var text = new Blotter.Text("Криэйтив", {
+var text = new Blotter.Text("Криэйтив 🥚", {
   family : "'PT Mono', serif",
-  size : 100,
+  size : 80,
   fill : "#171717",
   paddingLeft : 40,
   paddingRight : 40
@@ -9,7 +9,7 @@ var text = new Blotter.Text("Криэйтив", {
 
 var text2 = new Blotter.Text("кодинг", {
   family : "'PT Mono', serif",
-  size : 50,
+  size : 30,
   fill : "#171717",
   paddingLeft : 40,
   paddingRight : 40
@@ -34,8 +34,34 @@ var blotter = new Blotter(material, {
   texts : [text,text2]
 });
 
-var elem = document.querySelector(".container");
+var elem = document.querySelector(".canvas-container");
 var scope = blotter.forText(text);
 var scope2 = blotter.forText(text2);
 scope.appendTo(elem);
 scope2.appendTo(elem);
+
+
+let winsize;
+    const calcWinsize = () => winsize = {width: window.innerWidth, height: window.innerHeight};
+    calcWinsize();
+    window.addEventListener('resize', calcWinsize);
+
+    const getMousePos = (ev) => {
+        let posx = 0;
+        let posy = 0;
+        if (!ev) ev = window.event;
+        if (ev.pageX || ev.pageY) {
+            posx = ev.pageX;
+            posy = ev.pageY;
+        }
+        else if (ev.clientX || ev.clientY) 	{
+            posx = ev.clientX + body.scrollLeft + docEl.scrollLeft;
+            posy = ev.clientY + body.scrollTop + docEl.scrollTop;
+        }
+        return {x: posx, y: posy};
+    }
+
+    let mousePos = {x: winsize.width/2, y: winsize.height/2};
+    window.addEventListener('mousemove', ev => {
+      mousePos = getMousePos(ev);
+    });
